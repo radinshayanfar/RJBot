27be190108aa255003d56270f15f7dcdb00fc4bf
Radin <radin.shayanfar@gmail.com>
2019-04-26 22:25:16 +0430

    URLHelper implemented.

---
+++ Helpers/URLHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Helper;
+
+
+class URLHelper
+{
+
+    /**
+     * @param $url string URL to be validated
+     * @return bool true if $url is an valid URL
+     */
+    public static function validateUrl($url) {
+        if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $url string URL to check it's host
+     * @param $host string Host
+     * @return bool true if $url host and $host is equal
+     */
+    public static function isHostEquals($url, $host) {
+        if ((parse_url($url, PHP_URL_HOST) === $host) && (self::validateUrl($url))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * URLHelper private constructor.
+     */
+    private function __construct()
+    {
+    }
+}
