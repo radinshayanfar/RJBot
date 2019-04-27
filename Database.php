d1d969c771d910310cd779e9cece2f9f9efacd70
Radin <radin.shayanfar@gmail.com>
2019-04-27 09:59:15 +0430

    Database implemented.

---
+++ Database.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class Database
+{
+    private $dbConnection;
+
+    /**
+     * Database constructor.
+     * @param $host string DB host
+     * @param $user string DB user
+     * @param $password string DB password
+     * @param $name string DB name
+     */
+    public function __construct($host, $user, $password, $name)
+    {
+        if ($this->dbConnection = mysqli_connect($host, $user, $password, $name)) ;
+        else
+            throw new Exception('Unable to connect to database');
+    }
+
+    function update_count_db($update)
+    {
+        $chat_id = $update['message']['chat']['id'];
+        $user_FName = $update['message']['chat']['first_name'];
+        $user_FName = mysqli_real_escape_string($this->dbConnection, $user_FName);
+        $query = "SELECT * FROM subs WHERE chat_id = {$chat_id} LIMIT 1";
+        $r = @mysqli_query($this->dbConnection, $query);
+        $num = mysqli_num_rows($r);
+        if ($num > 0) {
+            $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+            $row['count']++;
+            $query = "UPDATE subs SET count = ${row['count']}, last_time = CURRENT_TIMESTAMP WHERE chat_id = {$chat_id} LIMIT 1";
+            $r = @mysqli_query($this->dbConnection, $query);
+        } else {
+            $query = "INSERT INTO subs (chat_id, name) VALUES ({$chat_id}, '{$user_FName}')";
+            $r = @mysqli_query($this->dbConnection, $query);
+        }
+    }
+
+
+}
