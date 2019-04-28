89bf9935b6f64bca178cf556dfef9d52d1163dc6
Radin <radin.shayanfar@gmail.com>
2019-04-28 14:40:15 +0430

    callback data set tested.

--- Test/AlbumTest.php
+++ Test/AlbumTest.php
@@ -5,7 +5,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include_once('../Core/Album.php');
+include_once('../DatabaseConfig.php');
+include_once('../Database.php');
 
 $url = new \Core\URLRedirect('https://rjapp.me/ma/KN7LDa7J');
-$album = new \Core\Album($url);
+$dbc = new Database(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+var_dump($dbc->autoIncrementStart());
+$album = new \Core\Album($url, $dbc->autoIncrementStart());
 var_dump($album->getLinks());
