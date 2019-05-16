1ea0967453994f58751b33f3e6f30f8d92d62c75
Radin <radin.shayanfar@gmail.com>
2019-05-16 18:07:31 +0430

    requirements moved to separate file.

--- Bot.php
+++ Bot.php
@@ -6,20 +6,7 @@ header("Content-Type: application/json");
 
 define('API_TOKEN', file_get_contents('TOKEN'));
 
-require_once('TelegramAPI.php');
-require_once('Sendable.php');
-require_once('./Database/DatabaseConfig.php');
-require_once('./Database/Database.php');
-require_once('Helpers/DebugHelper.php');
-require_once('Helpers/URLHelper.php');
-require_once('Helpers/StringHelper.php');
-require_once('Helpers/Helper.php');
-require_once('./Core/URLRedirect.php');
-include_once('./Core/MediaType.php');
-require_once('./Core/Video.php');
-require_once('./Core/SingleAudio.php');
-require_once('./Core/Album.php');
-require_once('./Update/Action.php');
+require_once("requirements.php");
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
