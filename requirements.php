5607ef62808a26b89c518375b4c05f35d976e0d7
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-03-16 20:39:45 +0330

    Database.php moved to root directory and requirements.php file updated.

--- requirements.php
+++ requirements.php
@@ -1,16 +1,16 @@
 <?php
 
+require_once('vendor/autoload.php');
 require_once('TelegramAPI.php');
 require_once('Sendable.php');
-require_once('./Database/DatabaseConfig.php');
-require_once('./Database/Database.php');
+require_once('Database.php');
 require_once('Helpers/DebugHelper.php');
 require_once('Helpers/URLHelper.php');
 require_once('Helpers/StringHelper.php');
 require_once('Helpers/Helper.php');
-require_once('./Core/URLRedirect.php');
-include_once('./Core/MediaType.php');
-require_once('./Core/Video.php');
-require_once('./Core/SingleAudio.php');
-require_once('./Core/Album.php');
-require_once('./Update/Action.php');
+require_once('Core/URLRedirect.php');
+include_once('Core/MediaType.php');
+require_once('Core/Video.php');
+require_once('Core/SingleAudio.php');
+require_once('Core/Album.php');
+require_once('Update/Action.php');
