86b4c53a4c9d03aa5397efe07ae6bd31c3813d70
Radin Shayanfar <radin.shayanfar@gmail.com>
2021-08-14 22:50:35 +0430

    Getting currentMP3Url fixed.

--- Core/Audio.php
+++ Core/Audio.php
@@ -3,6 +3,8 @@
 
 namespace Core;
 
+use Exception;
+use Helper\DebugHelper;
 use Helper\StringHelper;
 
 include_once('Media.php');
@@ -29,7 +31,31 @@ abstract class Audio extends Media
      */
     private function processRJCurrentMP3URL()
     {
-        $var_pos = strpos($this->url->getData(), 'RJ.currentMP3Url');
-        return StringHelper::betweenQuotes($this->url->getData(), $var_pos);
+        $csrf = StringHelper::getCSRFFromMeta($this->url->getData());
+        $headers = [
+            'X-CSRF-Token: ' . $csrf,
+            'X-Requested-With: XMLHttpRequest',
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url->getRedirectedURL() . '?setup=1');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($ch);
+        if (curl_errno($ch) == CURLE_OPERATION_TIMEDOUT) throw new Exception($GLOBALS["_STR"]["ERRORS"]["timeout"]);
+        if (curl_errno($ch)) throw new Exception($GLOBALS["_STR"]["ERRORS"]["unknown"]);
+//        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200) throw new Exception('Unknown error occurred. Please try again in a moment');
+        curl_close($ch);
+
+        $currentMP3URL = json_decode($result, true)['currentMP3Url'];
+        if ($currentMP3URL === null) {
+            throw new Exception($GLOBALS["_STR"]["ERRORS"]["get_mp3_url_error"]);
+        }
+        return $currentMP3URL;
     }
 }
